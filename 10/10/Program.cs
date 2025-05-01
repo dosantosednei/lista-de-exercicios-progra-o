e43249d2160e44f584ca05bb2f8b8b11cd1ea65f
dosantosednei <ednei.s@aluno.ifsp.edu.br>
2025-04-30 22:40:56 -0300

    Add files via upload

---
+++ 10/10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite os dois valores :");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+            
+            
+
+            
+
+            double mediageometrica = Math.Sqrt(valor1 * valor2);
+
+            Console.WriteLine("A media geometrica é :{0}",mediageometrica);
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 12/12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite os valores dos produtos");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+            double valor3 = Convert.ToDouble(Console.ReadLine());
+            double valor4 = Convert.ToDouble(Console.ReadLine());
+            double valor5 = Convert.ToDouble(Console.ReadLine());
+            double soma;
+
+            soma = valor1 + valor2 + valor3 + valor4 + valor5;
+
+            Console.WriteLine("O valor dos produtos é: {0} ", soma);
+
+            Console.WriteLine("digite o valor do pagamento");
+            double pag = Convert.ToDouble(Console.ReadLine());
+
+            double troco;
+
+            troco = pag - soma;
+
+            Console.WriteLine("O troco é:{0} ", troco);
+            Console.ReadKey();
+
+        }
+    }
+}
